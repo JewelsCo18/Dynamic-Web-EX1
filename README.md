5276eb0d3d0dc317741cbd9fe8705fc60753bce2
Jules Kho <juleskho@10-18-173-75.dynapool.wireless.nyu.edu>
2021-09-14 08:44:14 -0400

    adding container files

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Terminal Commands: 
 - to end program -> ctrl + c
+
+## To push up to Github (simple) 
+
+1. git add. 
+2. git commit -m "_message_"
+3. git push origin main
+
+
---
+++ src/containers/articleListing.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function articleListing(){
+    const articleListingsTitle = "Multiple Articles"; 
+    return (
+        <div>
+            <h1>{articleListingsTitle}</h1>
+        </div>
+    )
+}
+
+export default articleListing
