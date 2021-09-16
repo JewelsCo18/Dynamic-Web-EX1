4f57afc09fcc7e534713ade849e63e5be41b0751
Jules Kho <juleskho@10-18-173-75.dynapool.wireless.nyu.edu>
2021-09-16 09:45:58 -0400

    article listings page initial

--- src/containers/articleListing.js
+++ src/containers/articleListing.js
@@ -1,11 +1,21 @@
 import React from 'react'; 
+import ArticleCard from '../components/ArticleCard'
+import Data from '../components/data.json'
 
 function ArticleListing(){
-    const articleListingsTitle = "Multiple Articles"; 
+    const articleListingsTitle = "Articles"; 
+
+    console.log(Data); 
+
     return (
         <div className = "pageWrapper"> 
             {/* Must use className instead of class because it is a protect javascript word */}
             <h1 className = "HeaderOneStyle" >{articleListingsTitle}</h1>
+            {Data.map((article, key) => (
+                <ArticleCard article={article} key={key}/>
+            ))} 
+            {/* article and key are props that passes data from one parent component to a child component*/}
+            {/* must use curly braces for any execution of javascript*/}
         </div>
     )
 }
